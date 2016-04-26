6df90501235ad1dbbe89635a064cf10059418bfb
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-04-27 00:29:20 +0800

    inject angular

--- spinner.js
+++ spinner.js
@@ -1,55 +1,52 @@
-(function() {
+var angular = require('angular');
 
-  'use strict';
+angular
+  .module('spinner', [])
+  .directive('spinner', SpinnerDirective)
+  .factory('Spinner', SpinnerFactory);
 
-  angular
-    .module('spinner', [])
-    .directive('spinner', SpinnerDirective)
-    .factory('Spinner', SpinnerFactory);
+function SpinnerDirective() {
 
-  function SpinnerDirective() {
+  return {
+    restrict: 'EA',
+    scope: {
+      's': '=spinOn',
+    },
+    replace: true,
+    transclude: true,
+    template: '<ng-transclude ng-show="s.isSpinning()"></ng-transclude>',
+  };
 
-    return {
-      restrict: 'EA',
-      scope: {
-        's': '=spinOn',
-      },
-      replace: true,
-      transclude: true,
-      template: '<ng-transclude ng-show="s.isSpinning()"></ng-transclude>',
+}
+
+function SpinnerFactory() {
+
+  function Spinner() {
+    this.counter = 0;
+  }
+
+  Spinner.prototype.watch = watch;
+  Spinner.prototype.isSpinning = isSpinning;
+
+  function watch(q) {
+    this.counter++;
+    q.then(complete(this, q), complete(this, q));
+    return q;
+  }
+
+  var x = 0;
+
+  function isSpinning() {
+    return !!this.counter;
+  }
+
+  // utils
+
+  function complete(instance, q) {
+    return function() {
+      instance.counter--;
     };
-
   }
 
-  function SpinnerFactory() {
-
-    function Spinner() {
-      this.counter = 0;
-    }
-
-    Spinner.prototype.watch = watch;
-    Spinner.prototype.isSpinning = isSpinning;
-
-    function watch(q) {
-      this.counter++;
-      q.then(complete(this, q), complete(this, q));
-      return q;
-    }
-
-    var x = 0;
-
-    function isSpinning() {
-      return !!this.counter;
-    }
-
-    // utils
-
-    function complete(instance, q) {
-      return function() {
-        instance.counter--;
-      };
-    }
-
-    return Spinner;
-  }
-})();
+  return Spinner;
+}
